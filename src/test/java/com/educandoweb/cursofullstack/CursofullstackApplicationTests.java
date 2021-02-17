4fa988473c054e29bac2921b1768a116d470918a
Mauro Rezende <maurorezende2012@gmail.com>
2021-02-17 17:56:36 -0300

    Projeto Criado

---
+++ src/test/java/com/educandoweb/cursofullstack/CursofullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.cursofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursofullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
