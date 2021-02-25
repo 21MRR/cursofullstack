163b47b8990fa0476228b4faba22d56a4dd36620
Mauro Rezende <maurorezende2012@gmail.com>
2021-02-25 17:53:18 -0300

    Operação de Instanciação

--- src/main/java/com/educandoweb/cursofullstack/CursofullstackApplication.java
+++ src/main/java/com/educandoweb/cursofullstack/CursofullstackApplication.java
@@ -1,13 +1,34 @@
 package com.educandoweb.cursofullstack;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.educandoweb.cursofullstack.domain.Categoria;
+import com.educandoweb.cursofullstack.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursofullstackApplication {
+public class CursofullstackApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursofullstackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 
 }
