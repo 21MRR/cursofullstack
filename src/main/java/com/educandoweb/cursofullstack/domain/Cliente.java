93e95f4e57e8745e0199ddf0c15300f9eb9d409d
Mauro Rezende <maurorezende2012@gmail.com>
2021-05-26 17:46:16 -0300

    Inserindo nova categoria POST, PUT, GET E DELETE

--- src/main/java/com/educandoweb/cursofullstack/domain/Cliente.java
+++ src/main/java/com/educandoweb/cursofullstack/domain/Cliente.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 
 import com.educandoweb.cursofullstack.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
 @Entity
@@ -32,7 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
+	
     @OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
     
--- src/main/java/com/educandoweb/cursofullstack/services/CategoriaService.java
+++ src/main/java/com/educandoweb/cursofullstack/services/CategoriaService.java
@@ -11,14 +11,32 @@ import com.educandoweb.cursofullstack.services.exception.ObjectNotFoundException
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		}
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+
+	}
+
+	public Categoria update(Categoria obj) {
+		buscar(obj.getId());
+		return repo.save(obj);
+
+	}
+
+	public void delete(Integer id) {
+		buscar(id);
+		repo.deleteById(id);
+		
+	}
 
 }
